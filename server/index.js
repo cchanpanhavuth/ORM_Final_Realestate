c6120b75446248fe9ef1e577fd52f0920373ebbd
Neathhhh <129174273+Neathhhh@users.noreply.github.com>
2024-07-31 19:53:09 +0700

    Create sign up API route

--- server/index.js
+++ server/index.js
@@ -1,8 +1,13 @@
 import {PrismaClient} from '@prisma/client'
+import authRouter from './routes/auth.route.js'
+import userRouter from './routes/user.route.js'
 import  express from 'express';
 const app = express();
 const prisma = new PrismaClient()
-app.listen(3000, () =>{
-    console.log('Server is running on port 3000');
+app.use(express.json())
+app.listen(8080, () =>{
+    console.log('Server is running on port 8080');
 }
 );
+ app.use('/api/auth', authRouter)
+ app.use('/api/user', userRouter)
