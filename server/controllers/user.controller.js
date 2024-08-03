71386ef878d93990c0f0e5a5c404d0b279906e4a
cchanpanhavuth <chanpanhavuth.chea@gmail.com>
2024-08-03 13:11:37 +0700

    user update api

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,3 +1,35 @@
+import { PrismaClient } from "@prisma/client";
+import { errorHandler } from "../utils/error.handler.js"
+import bcrypt from 'bcrypt'
+
+const prisma = new PrismaClient();
+
 export const test = (req,res) =>{
     res.send('Hello Guyssss')
 }
+
+export const updateUser = async (req, res, next) => {
+    if(req.user.id !== req.params.id){ 
+        return next(errorHandler(401, "You can only update your own account"))
+    }
+    try{
+        if(req.body.password){
+            req.body.password = bcrypt.hashSync(req.body.password, 10)
+        }
+
+        const updatedUser = await prisma.users.update({
+            where: { id: req.params.id },
+            data: {
+                username: req.body.username,
+                email: req.body.email,
+                password: req.body.password,
+                avatar: req.body.avatar
+            }
+        });
+        const {password, ...others} = updatedUser
+
+        res.status(200).json(others)
+    }catch(error){
+        next(error)
+    }
+};
