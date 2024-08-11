ab1b3a3786cae5136340c357b2477be6e3b49f8e
Neathhhh <129174273+Neathhhh@users.noreply.github.com>
2024-08-11 23:43:54 +0700

    complete update listing functionality

--- server/routes/listing.route.js
+++ server/routes/listing.route.js
@@ -1,14 +1,18 @@
 import express from 'express';
-import { getAllListing, createListing, getListingType, deleteListing} from '../controllers/listing.controller.js';
+import { getAllListing, getListings, createListing, getListingType, deleteListing, updateListing, getListings} from '../controllers/listing.controller.js';
 import { verifyUser } from '../utils/verifyUser.js';
 
 const router = express.Router();
 
 
-router.get('/views',verifyUser,  getAllListing);
+
 router.get('/type',  getListingType);
+router.get('/getListings',verifyUser,  getAllListing);
+router.get('/getListing/:id',  getListings);
 router.post('/create', verifyUser, createListing);
 router.delete('/delete/:id', verifyUser, deleteListing);
+router.patch('/update/:id', verifyUser, updateListing);
+
 
 
 export default router;
